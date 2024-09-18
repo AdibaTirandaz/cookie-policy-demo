118f40dadcbae479c7c907d504350be90084cff0
tirandazelif <tirandazelif@gmail.com>
2024-09-19 04:58:03 +0530

    Updates

---
+++ precache-manifest.8dc0fa88cc0d05cfe958cd630cde12e0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8812c1e6625913758102331f9f38efb7",
+    "url": "/cookie-policy-demo/index.html"
+  },
+  {
+    "revision": "37d6532fc5fd3eaa7096",
+    "url": "/cookie-policy-demo/static/css/2.4864c097.chunk.css"
+  },
+  {
+    "revision": "7453757b88279bf5b7f4",
+    "url": "/cookie-policy-demo/static/css/main.8a418433.chunk.css"
+  },
+  {
+    "revision": "37d6532fc5fd3eaa7096",
+    "url": "/cookie-policy-demo/static/js/2.76aa52f1.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/cookie-policy-demo/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "7453757b88279bf5b7f4",
+    "url": "/cookie-policy-demo/static/js/main.67b2b6a2.chunk.js"
+  },
+  {
+    "revision": "f48d1a484f454bce856a",
+    "url": "/cookie-policy-demo/static/js/runtime~main.a462b26f.js"
+  },
+  {
+    "revision": "13f6bac53033dd05f1445cf526972f30",
+    "url": "/cookie-policy-demo/static/media/61fc00073cf0e70004222069.13f6bac5.png"
+  },
+  {
+    "revision": "3290338e47901d47380de77b14038536",
+    "url": "/cookie-policy-demo/static/media/Arrow_Up.3290338e.svg"
+  },
+  {
+    "revision": "c7dc1e840df3fbb12569d3aaf21f8de7",
+    "url": "/cookie-policy-demo/static/media/Dash.c7dc1e84.PNG"
+  },
+  {
+    "revision": "40ec3f11f3cc03433ef80cdf1c30159c",
+    "url": "/cookie-policy-demo/static/media/Priavcy-Notice-Hindi (1).40ec3f11.pdf"
+  },
+  {
+    "revision": "6f33ace0d786b944f94c3b64c6761c2f",
+    "url": "/cookie-policy-demo/static/media/Privacy Notice_SBI copy.6f33ace0.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cookie-policy-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cookie-policy-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cookie-policy-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cookie-policy-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cookie-policy-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
